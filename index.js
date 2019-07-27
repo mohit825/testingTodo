b50a133b52433a0d462a74c8960cc80ea6c4c9fe
Mohit Pandey <mohitpandey825@gmail.com>
2019-07-27 22:07:08 +0530

    initial

---
+++ index.js
@@ -0,0 +1,32 @@
+onAdd = (e) => {
+    e.preventDefault();
+    let val = (e.target[0].value);
+    if(val){
+        const li = document.createElement("li");
+        const del = document.createElement("button");
+        del.innerHTML="delete";
+        const btn = document.createElement("button");
+        btn.innerHTML="edit";
+    
+        const ul = document.querySelector("ul");
+        console.log(ul);
+        
+        const div = document.createElement("div")
+        console.log(e);
+    
+    const todo = ul.appendChild(li).appendChild(div).innerHTML=val;
+    li.appendChild(btn);
+    li.appendChild(del);
+    del.addEventListener("click" , ()=>{
+        li.remove();
+    })
+    document.querySelector("input").value="";
+    
+    }
+
+
+
+    
+    
+    
+} 
